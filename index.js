bd9e8e926d15a6d6735b88be34df4e50806bb24c
Chris Lane <chris@chris-allen-lane.com>
2020-06-17 11:25:45 -0400

    feat: display latest release

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const exec = require('child_process').exec;
 
 exec('git rev-parse HEAD', function(err, stdout) {
-    console.log('latest hash:', stdout.trim());
+  console.log('latest hash:\t', stdout.trim());
+
+  exec('git describe', function(err, stdout) {
+    console.log('latest release:\t', stdout.trim());
+  });
 });
